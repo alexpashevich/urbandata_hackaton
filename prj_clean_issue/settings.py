c630232e68562506b85233b77b0647ee0924a92b
alexpashevich <alexpashevich@github.com>
2015-04-24 22:38:15 +0300

    bug fixed

--- prj_clean_issue/settings.py
+++ prj_clean_issue/settings.py
@@ -74,16 +74,18 @@ WSGI_APPLICATION = 'prj_clean_issue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+import dj_database_url
+DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': dj_database_url.config()
+}
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
