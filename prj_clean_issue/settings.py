4279d840a4896c200443689138fb2a54fb5e0118
alexpashevich <alexpashevich@github.com>
2015-04-25 23:14:56 +0300

    static fix

--- prj_clean_issue/settings.py
+++ prj_clean_issue/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'timeschedule_analysis/static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
