1d43584ef92db2a30baaac9545cfda2b51cc5afd
alexpashevich <alexpashevich@github.com>
2015-04-25 15:31:52 +0300

    models fix

--- timeschedule_analysis/models.py
+++ timeschedule_analysis/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
 class City(models.Model):
-    city_id = models.IntegerField(unique=True)
     name = models.TextField()
     # question_text = models.CharField(max_length=200)
     # pub_date = models.DateTimeField('date published')
 
 class Bin(models.Model):
-    bin_id = models.IntegerField(unique=True)
     x_coordiante = models.FloatField()
     y_coordinate = models.FloatField()
     address = models.TextField()
     volume = models.IntegerField()
-    city = models.ForeignKey(City)
+    # city = models.ForeignKey(City)
 
 class Sample(models.Model):
-    sample_id = models.IntegerField(unique=True)
     bin = models.ForeignKey(Bin)
     date = models.DateField()
     volume_old = models.IntegerField()
