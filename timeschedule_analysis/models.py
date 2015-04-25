df7fb6f65e9f9e8767fb4b1494a618df8bb25580
alexpashevich <alexpashevich@github.com>
2015-04-25 15:42:59 +0300

    db changed

--- timeschedule_analysis/models.py
+++ timeschedule_analysis/models.py
@@ -4,19 +4,25 @@ class City(models.Model):
     name = models.TextField()
     # question_text = models.CharField(max_length=200)
     # pub_date = models.DateTimeField('date published')
+    def __str__():
+        return self.name
 
 class Bin(models.Model):
     x_coordiante = models.FloatField()
     y_coordinate = models.FloatField()
     address = models.TextField()
     volume = models.IntegerField()
-    # city = models.ForeignKey(City)
+    city = models.ForeignKey(City)
+    def __str__():
+        return "Container with address " + self.address
 
 class Sample(models.Model):
     bin = models.ForeignKey(Bin)
     date = models.DateField()
     volume_old = models.IntegerField()
     volume_new = models.IntegerField()
+    def __str__():
+        return "Sample of the container " + self.bin.address + ' ' + self.bin.date
 
 # class Choice(models.Model):
     # question = models.ForeignKey(Question)
