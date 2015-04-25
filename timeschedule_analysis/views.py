a7693aab3f897d41bbec3e03364aa8f184a74219
alexpashevich <alexpashevich@github.com>
2015-04-26 01:49:26 +0300

    views.py changed

--- timeschedule_analysis/views.py
+++ timeschedule_analysis/views.py
@@ -26,23 +26,20 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 def get_all_bins(request):
-    # if request.method == 'GET':
-    logger.info('get_all_bins function')
+    # logger.info('get_all_bins function')
     bins = Bin.objects.all()
     bins_array = []
-    for bin in bins:
+    for bin in Bin.objects.all():
         bin_dict = {}
         bin_dict['x_coordinate'] = bin.x_coordinate
         bin_dict['y_coordinate'] = bin.y_coordinate
         bin_dict['address'] = bin.address
         bin_dict['volume'] = bin.volume
         bin_dict['cur_filling'] = bin.cur_filling
-        logger.info(bin)
-        # logger.info(bin.x_coordinate)
-        # logger.info(bin.y_coordinate)
         bins_array.append(bin_dict)
-    return JsonResponse(bins_array)
-    # return HttpResponse(json.dumps({'a': 1}), content_type='application/json')
+    # logger.info(bins_array)
+    dump = json.dumps(bins_array)
+    return HttpResponse(dump, content_type='application/json')
 
 def send_new_bin(request):
     logger.info('send_new_bin function')
