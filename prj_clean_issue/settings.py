063526c9b61e3ab93c9ec83c144f4d206e3f8cc0
alexpashevich <alexpashevich@github.com>
2015-04-25 23:12:46 +0300

    static fix

--- prj_clean_issue/settings.py
+++ prj_clean_issue/settings.py
@@ -145,8 +145,8 @@ LOGGING = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
