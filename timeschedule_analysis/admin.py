6da1c9f98f4cc1a93ee49de2f6f28094c3b1d72f
alexpashevich <alexpashevich@github.com>
2015-04-25 15:12:40 +0300

    db changed

--- timeschedule_analysis/admin.py
+++ timeschedule_analysis/admin.py
@@ -4,3 +4,5 @@ import timeschedule_analysis.models
 # Register your models here.
 
 admin.site.register(timeschedule_analysis.models.Bin)
+admin.site.register(timeschedule_analysis.models.City)
+admin.site.register(timeschedule_analysis.models.Sample)
